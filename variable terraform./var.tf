7c8804f93e1156c7f7653dc4114add182c047930
Praveenchoudary <96223263+Praveenchoudary@users.noreply.github.com>
2024-09-23 23:47:07 +0530

    Create var.tf

---
+++ variable terraform./var.tf
@@ -0,0 +1,10 @@
+variable "amiid" {
+  description = "This is amiid"
+  type        = string
+  default     = "ami-06f855639265b5541"
+}
+
+variable "itype" {
+  type    = string
+  default = "t2.micro"
+}
